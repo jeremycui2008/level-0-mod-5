a6b5d1579f78aa65d9098f2d247a7cc78e484c26
Marcus Solt <marcussolt723@gmail.com>
2022-09-02 19:53:25 -0700

    cool

--- _01_function_writing/_d_happy_pet.py
+++ _01_function_writing/_d_happy_pet.py
@@ -4,11 +4,42 @@ Write methods to represent the activities that will make the user's pet happy.
 from tkinter import messagebox, simpledialog, Tk
 
 
+def walk(cyanide):
+    if cyanide=='fish':
+        messagebox.showinfo(None, message='you killed it')
+        exit()
+    if cyanide=='dog':
+        messagebox.showinfo(None, message='they are more more happy now')
+    if cyanide=='cat':
+        messagebox.showinfo(None, message='they are happier now')
+
+
 if __name__ == '__main__':
-    # TODO)
-    #   1. Ask the user to enter the type of pet they want (give them a few
-    #      choices).
-    messagebox.showinfo(None, prompt='which pet do you want (cat,dog,fish)')
+# TODO)
+#   1. Ask the user to enter the type of pet they want (give them a few
+#      choices).
+    window = Tk()
+    window.withdraw()
+    happiness=10
+    cyanide=simpledialog.askstring(None, prompt='which pet do you want (cat,dog,fish)')
+    slavery=True
+    while slavery:
+        what=simpledialog.askstring(None, prompt='what you wanna do with the pet')
+        if what=='walk':
+            walk(cyanide)
+            happiness+=5;
+        if happiness==100:
+            messagebox.showinfo(None, message='your pet might be a bit too excited, you should let it calm down.')
+        if happiness>100:
+            messagebox.showinfo(None, message='your pet got too excited and died')
+            exit()
+
+
+
+
+
+
+
 
 
     #   2. Use a loop (maybe a while loop?) to keep offering interactions with
